456b33e064a0984f244da76d661bf5bb4965e8eb
Mitaric <40798963+Mitaric@users.noreply.github.com>
2020-03-20 17:00:40 -0400

    Update pihole.sh

--- pihole.sh
+++ pihole.sh
@@ -3,5 +3,7 @@
 apt update
 apt upgrade -y
 apt install curl -y
-curl -sSL https://install.pi-hole.net | bash
-
+curl -sSL https://install.pi-hole.net -o install-pihole.sh
+chmod +x install-pihole.sh
+source install-pihole.sh --unattended
+rm install-pihole.sh
